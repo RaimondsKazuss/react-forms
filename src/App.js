224a13956bb61a2f59f9b0db186ade49f57a9f3e
Raimonds Kazuss <rkazush@gmail.com>
2022-03-09 18:47:53 +0200

    task 1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const FormContainer = styled.div`
@@ -9,11 +10,25 @@ const FormContainer = styled.div`
 `
 
 function App() {
+  const [formState, setFormState] = useState({
+    name: '',
+    lastName: ''
+  })
+
+  console.log(formState);
+
+  
   return (
     <div className="App">
       <FormContainer>
         <form>
-          <input type="text"/>
+          <label>
+            Name:
+            <input type="text" value={formState.name} onChange={(event => setFormState({...formState, name: event.target.value}))}/>
+            <input type="text" value={formState.lastName} onChange={(event => setFormState({...formState, lastName: event.target.value}))}/>
+            
+          </label>
+          
         </form>  
       </FormContainer>      
     </div>
